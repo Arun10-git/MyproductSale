0d0c1f50483559ae3a383c16f1f88318a2489caa
Arun10-git <aru02027@gmail.com>
2026-03-31 12:36:19 +0530

    fix errors

--- productsales/settings.py
+++ productsales/settings.py
@@ -15,7 +15,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "myproductsale-9.onrender.com,localhost,127.0.0.1"
+    "myproductsale-10.onrender.com,localhost,127.0.0.1"
 ).split(",")
 
 
