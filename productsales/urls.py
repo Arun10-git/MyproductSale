c5c36382b876e72d67935ecbbd6658bc0e26ffb6
Arun10-git <aru02027@gmail.com>
2026-03-31 14:38:03 +0530

    fixed urls

--- productsales/urls.py
+++ productsales/urls.py
@@ -1,8 +1,14 @@
+from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.views.generic import RedirectView
 
+def home(request):
+    return HttpResponse("My Product Sale App is Live 🚀")
+
 urlpatterns = [
+    path('', home), 
+    path('admin/', admin.site.urls),
     path('product/', include('products.urls')),
     path('auth/', include('authentication.urls')),
-    path('', RedirectView.as_view(url='/products/', permanent=True))
 ]
